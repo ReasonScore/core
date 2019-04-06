0b5ed7418da4713cd9392987767a87bb59c9295c
Bentley Davis <Bentley@BentleyDavis.com>
2019-04-06 12:24:43 -0500

    Added web Server and change evrsion to 0

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+var PACKAGE = require('./package.json');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'ReasonScoreCore-1.js',
+        filename: 'ReasonScoreCore-' + PACKAGE.version.split('.')[0] + '.js',
         library: 'ReasonScoreCore',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8000
+    },
 };
