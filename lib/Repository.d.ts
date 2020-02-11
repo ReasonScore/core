a95d13d8c5aeaddbd0d3a9a06d7920aa6baf6868
Bentley Davis <Bentley@BentleyDavis.com>
2020-02-10 19:38:06 -0600

    npm audit and publish

--- lib/Repository.d.ts
+++ lib/Repository.d.ts
@@ -16,5 +16,5 @@ export declare class Repository implements iRepository {
     getItem(ItemId: Id, when?: string): Promise<Item | undefined>;
     getClaimEdgesByParentId(parentId: Id, when?: string): Promise<ClaimEdge[]>;
     getClaimEdgesByChildId(childId: Id, when?: string): Promise<ClaimEdge[]>;
-    getScoreBySourceClaimId(sourceClaimId: Id, when?: string): Promise<Score>;
+    getScoreBySourceClaimId(sourceClaimId: Id, when?: string): Promise<Score | undefined>;
 }
